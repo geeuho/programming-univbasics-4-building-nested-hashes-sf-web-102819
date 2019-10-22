cf74162c7feb0fd2f52faf47cc4c1eac8d4afbd4
Allen Shin <geeuho@gmail.com>
2019-10-22 20:31:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,18 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  monopoly = {
+monopoly = {
+    
     :railroads => {
       :pieces => 4
     }
     
-  }
+}
 end
 
 def monopoly_with_third_tier
-  monopoly = {  
+
+monopoly = {  
   :railroads => {
     
     :pieces => 4,
